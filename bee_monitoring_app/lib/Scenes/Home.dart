ccbd10e37b5a1c62d16f660bb49774050674607a
João Flores <j199818@dac.unicamp.br>
2022-10-28 00:26:06 -0300

    V1 finalizada

--- bee_monitoring_app/lib/Scenes/Home.dart
+++ bee_monitoring_app/lib/Scenes/Home.dart
@@ -5,34 +5,46 @@ class Home extends StatelessWidget {
   final List<Item> data;
   Home(this.data);
 
-  List<Type> temperature = [Type.temperature, Type.humidity, Type.sound];
-
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
         separatorBuilder: (BuildContext context, int index) => const Divider(),
-        itemCount: temperature.length,
+        itemCount: Type.values.length + 1,
         itemBuilder: (context, index) {
-          return Card(
-              child: ListTile(title: Text(getAverage(temperature[index], data)), subtitle: Text(temperature[index].value)));
+          return index == 0
+              ? 
+              Padding(
+  padding: EdgeInsets.only(left:15, bottom: 10, right: 20, top:20), //apply padding to some sides only
+  child:               Text(
+                  "Médias",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold),
+                )
+)
+              : Card(
+                  child: ListTile(
+                      title: Text(getAverage(Type.values[index - 1], data)),
+                      subtitle: Text(Type.values[index - 1].value)));
         });
   }
 
-  String getAverage(Type type, List<Item> myParam) {
+  String getAverage(Type type, List<Item> dataArray) {
     var sum = 0;
-    for (var i = 0; i < myParam.length; i++) {
+    for (var i = 0; i < dataArray.length; i++) {
       switch (type) {
-      case Type.temperature:
-        sum += int.parse(myParam[i].temperature);
-        break;
-      case Type.humidity:
-        sum += int.parse(myParam[i].humidity);
-        break;
-      case Type.sound:
-        sum += int.parse(myParam[i].sound);
-        break;
+        case Type.temperature:
+          sum += int.parse(dataArray[i].temperature);
+          break;
+        case Type.humidity:
+          sum += int.parse(dataArray[i].humidity);
+          break;
+        case Type.sound:
+          sum += int.parse(dataArray[i].sound);
+          break;
+      }
     }
-    }
-    return (sum / myParam.length).toStringAsFixed(2);
-  }  
+    return (sum / dataArray.length).toStringAsFixed(2);
+  }
 }
