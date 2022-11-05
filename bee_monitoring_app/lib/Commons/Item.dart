a933e45ea044ecf82e4d7d5da70d5e736cdba9ce
João Flores <j199818@dac.unicamp.br>
2022-11-04 23:28:39 -0300

    Checkbox para seleção de dados  a serem  apresentados no gráfico

--- bee_monitoring_app/lib/Commons/Item.dart
+++ bee_monitoring_app/lib/Commons/Item.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
 enum Type {
   temperature("Temperatura"),
   humidity("umidade"),
@@ -16,3 +20,92 @@ class Item {
 
   const Item(this.id, this.temperature, this.humidity, this.sound, this.timestamp);
 }
+
+// -------------------------------------
+
+// class MyCartesianChart extends StatefulWidget {
+//   const MyCartesianChart({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyCartesianChart> createState() => _MyCartesianChartState();
+// }
+
+// class _MyCartesianChartState extends State<MyCartesianChart> {
+//   //3set data
+//   late List<SalesData> _chartData;
+
+//   @override
+//   void initState() {
+//     //4,initialized
+//     _chartData = getChartData();
+//     log('Chat Data-->$_chartData');
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: const Color.fromRGBO(0, 150, 136, 1),
+//           title: const Text('Cartesian Chart'),
+//         ),
+//         body: SfCartesianChart(
+//           enableAxisAnimation: true,
+//           primaryXAxis: CategoryAxis(),
+//           title: ChartTitle(text: 'Data analysis'),
+//           legend: Legend(
+//             isVisible: true,
+//           ),
+//           tooltipBehavior: TooltipBehavior(enable: true),
+//           series: <LineSeries<SalesData, String>>[
+//             LineSeries<SalesData, String>(
+//               // enableTooltip: true,
+//               name: 'sales',
+//               dataSource: _chartData,
+//               xValueMapper: (SalesData sales, _) => sales.month,
+//               yValueMapper: (SalesData sales, _) => sales.sales,
+//               dataLabelSettings: const DataLabelSettings(
+//                 isVisible: true,
+//                 color: Colors.blue,
+//               ),
+//             ),
+//             LineSeries(
+//               name: 'purchase',
+//               dataSource: _chartData,
+//               xValueMapper: (SalesData sales, _) => sales.month,
+//               yValueMapper: (SalesData sales, _) => sales.purchase,
+//               dataLabelSettings: const DataLabelSettings(
+//                 isVisible: true,
+//                 color: Color.fromARGB(255, 126, 19, 19),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   //2, get
+//   List<SalesData> getChartData() {
+//     final List<SalesData> chartData = [
+//       SalesData('January', 11, 14),
+//       SalesData('February', 14, 19),
+//       SalesData('March', 23, 12),
+//       SalesData('April', 12, 31),
+//       SalesData('May', 30, 11),
+//       SalesData('Jun', 25, 15),
+//     ];
+//     return chartData;
+//   }
+// }
+
+//1.create model class for data
+class SalesData {
+  SalesData(this.month, this.sales, this.purchase, this.teste);
+  final String month;
+  final double sales;
+  final double purchase;
+  final double teste;
+}
+  
--- bee_monitoring_app/lib/Scenes/Home.dart
+++ bee_monitoring_app/lib/Scenes/Home.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:bee_monitoring_app/Commons/Item.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'dart:developer';
 
 class Home extends StatelessWidget {
   final List<Item> data;
   Home(this.data);
+
+  List<SalesData> getChartData() {
+    final List<SalesData> chartData = [
+      SalesData('Jan', 11, 14, 14),
+      SalesData('Feb', 14, 19, 14),
+      SalesData('Mar', 23, 12, 14),
+      SalesData('Apr', 12, 31, 14),
+      SalesData('May', 30, 11, 14),
+      SalesData('Jun', 25, 15, 14),
+    ];
+    return chartData;
+  }
 
   List<String> titles = ["Médias", "Máximas", "Mínimas"];
   @override
@@ -12,10 +26,109 @@ class Home extends StatelessWidget {
         separatorBuilder: (BuildContext context, int index) => const Divider(),
         itemCount: (Type.values.length + 1) * titles.length,
         itemBuilder: (context, index) {
-          return index % (Type.values.length + 1) == 0
-              ? createTitle(index)
-              : createCard(index);
+          return index < 1 ? createCheckbox() : createCell(index - 1);
         });
+  }
+
+  late List<SalesData> _chartData;
+  Row createCheckbox() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            flex: 5,
+            child: CheckboxListTile(
+              title: Text("title text"),
+              value: true,
+              onChanged: (newValue) {
+                print("oi");
+              },
+              controlAffinity:
+                  ListTileControlAffinity.leading, //  <-- leading Checkbox
+            )),
+        Expanded(
+            flex: 5,
+            child: CheckboxListTile(
+              title: Text("title text"),
+              value: true,
+              onChanged: (newValue) {
+                print("oi");
+              },
+              controlAffinity:
+                  ListTileControlAffinity.leading, //  <-- leading Checkbox
+            ))
+      ],
+    );
+
+    // CheckboxListTile(
+    //         title: Text("helo"),
+    //         controlAffinity: ListTileControlAffinity.leading,
+    //         value: true,
+    //         onChanged: null,
+
+    //       ),
+    //     ),
+
+//     CheckboxListTile(
+//   title: Text("title text"),
+//   value: true,
+//   onChanged: (newValue) {
+//       print("oi");
+//   },
+//   controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
+// );
+  }
+
+  SfCartesianChart createChart() {
+    _chartData = getChartData();
+    return SfCartesianChart(
+      enableAxisAnimation: true,
+      primaryXAxis: CategoryAxis(),
+      title: ChartTitle(text: '11 jan - 18 jan'),
+      legend: Legend(
+        isVisible: true,
+        position: LegendPosition.bottom,
+      ),
+      tooltipBehavior: TooltipBehavior(enable: true),
+      series: <LineSeries<SalesData, String>>[
+        LineSeries<SalesData, String>(
+          enableTooltip: true,
+          name: 'sales',
+          dataSource: _chartData,
+          xValueMapper: (SalesData sales, _) => sales.month,
+          yValueMapper: (SalesData sales, _) => sales.sales,
+          dataLabelSettings: const DataLabelSettings(
+            isVisible: true,
+            color: Colors.blue,
+          ),
+        ),
+        LineSeries(
+          name: 'purchase',
+          dataSource: _chartData,
+          xValueMapper: (SalesData sales, _) => sales.month,
+          yValueMapper: (SalesData sales, _) => sales.purchase,
+          dataLabelSettings: const DataLabelSettings(
+            isVisible: true,
+            color: Color.fromARGB(255, 126, 19, 19),
+          ),
+        ),
+        LineSeries(
+          name: 'teste',
+          dataSource: _chartData,
+          xValueMapper: (SalesData sales, _) => sales.month,
+          yValueMapper: (SalesData sales, _) => sales.teste,
+          dataLabelSettings: const DataLabelSettings(
+            isVisible: true,
+            color: Color.fromARGB(255, 255, 0, 238),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Padding createCell(int index) {
+    return index % (Type.values.length + 1) == 0
+        ? createTitle(index)
+        : createCard(index);
   }
 
   Padding createTitle(int index) {
@@ -30,16 +143,29 @@ class Home extends StatelessWidget {
         ));
   }
 
-  Card createCard(int index) {
+  Padding createCard(int index) {
     if (index < (Type.values.length + 1)) {
-      return Card(
-          child: ListTile(title: Text(getAverage(Type.values[index - 1], data)), subtitle: Text(Type.values[index - 1].value)));
-    } if (index < (Type.values.length + 1)*2) {
-      return Card(
-          child: ListTile(title: Text(getMax(Type.values[index - 1 - 4], data)), subtitle: Text(Type.values[index - 1 - 4].value)));
+      return Padding(
+          padding: EdgeInsets.only(left: 15, bottom: 10, right: 20, top: 20),
+          child: Card(
+              child: ListTile(
+                  title: Text(getAverage(Type.values[index - 1], data)),
+                  subtitle: Text(Type.values[index - 1].value))));
+    }
+    if (index < (Type.values.length + 1) * 2) {
+      return Padding(
+          padding: EdgeInsets.only(left: 15, bottom: 10, right: 20, top: 20),
+          child: Card(
+              child: ListTile(
+                  title: Text(getMax(Type.values[index - 1 - 4], data)),
+                  subtitle: Text(Type.values[index - 1 - 4].value))));
     } else {
-      return Card(
-          child: ListTile(title: Text(getMin(Type.values[index - 1 - 8], data)), subtitle: Text(Type.values[index - 1 - 8].value)));
+      return Padding(
+          padding: EdgeInsets.only(left: 15, bottom: 10, right: 20, top: 20),
+          child: Card(
+              child: ListTile(
+                  title: Text(getMin(Type.values[index - 1 - 8], data)),
+                  subtitle: Text(Type.values[index - 1 - 8].value))));
     }
   }
 
@@ -47,7 +173,7 @@ class Home extends StatelessWidget {
     var sum = 0;
     for (var i = 0; i < dataArray.length; i++) {
       switch (type) {
-        case Type.temperature: 
+        case Type.temperature:
           if (sum < int.parse(dataArray[i].temperature)) {
             sum = int.parse(dataArray[i].temperature);
           }
@@ -71,7 +197,7 @@ class Home extends StatelessWidget {
     var sum = 1000;
     for (var i = 0; i < dataArray.length; i++) {
       switch (type) {
-        case Type.temperature: 
+        case Type.temperature:
           if (sum > int.parse(dataArray[i].temperature)) {
             sum = int.parse(dataArray[i].temperature);
           }
