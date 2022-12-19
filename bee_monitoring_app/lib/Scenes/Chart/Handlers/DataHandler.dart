8446347b642567caaceaf7eddcf9ac95100186a9
João Flores <j199818@dac.unicamp.br>
2022-12-19 18:35:51 -0300

    Refatoração

--- bee_monitoring_app/lib/Scenes/Chart/Handlers/DataHandler.dart
+++ bee_monitoring_app/lib/Scenes/Chart/Handlers/DataHandler.dart
@@ -31,73 +31,43 @@ extension DataHandler on _ChartViewControllerState {
     var initIndex = 0;
     var finalIndex = 0;
 
-    while (finalIndex+1 < dataStateTemp.length) {
-      while (firstDate == dataStateTemp[finalIndex+1].timestamp.day) {
+    while (finalIndex + 1 < dataStateTemp.length) {
+      while (firstDate == dataStateTemp[finalIndex + 1].timestamp.day) {
         finalIndex++;
-        if (finalIndex+1 >= dataStateTemp.length) {
+        if (finalIndex + 1 >= dataStateTemp.length) {
           finalIndex--;
           break;
         }
       }
-        print(initIndex);
-        print(finalIndex);
-
-        chartData.insert(
-            0,
-            ChartItem(
-                convertDateToString(dataStateTemp[initIndex].timestamp),
-                service.getAverage(Type.temperatureInside,
-                    dataStateTemp.sublist(initIndex, finalIndex)),
-                service.getAverage(Type.temperatureOutside,
-                    dataStateTemp.sublist(initIndex, finalIndex)),
-                service.getAverage(Type.humidityInside,
-                    dataStateTemp.sublist(initIndex, finalIndex)),
-                service.getAverage(Type.humidityOutside,
-                    dataStateTemp.sublist(initIndex, finalIndex)),
-                service.getAverage(
-                    Type.sound, dataStateTemp.sublist(initIndex, finalIndex))));
-
-          // break;
-          if (finalIndex+1 < dataStateTemp.length) {
-              finalIndex += 1; 
-              initIndex = finalIndex;
-              firstDate = dataStateTemp[initIndex].timestamp.day;
-          } else {
-            break;
-          }
-    }
-
-    while (chartData.length < 6) {
-      chartData.insert(0,ChartItem("",0,0,0,0,0));
-    }
-    return chartData;
-  }
-
-  List<ChartItem> handleAverageData2() {
-    final List<ChartItem> chartData = [];
-    List<Item> dataStateTemp = _allData.toList();
-
-    while (dataStateTemp.isNotEmpty) {
-      DateTime currentDate = dataStateTemp.first.timestamp;
-      List<Item> tempArray = [];
-
-      while (dataStateTemp.first.timestamp.day == currentDate.day) {
-        tempArray.add(dataStateTemp.first);
-        dataStateTemp.removeAt(0);
-        if (dataStateTemp.isEmpty) {
-          break;
-        }
-      }
+      print(initIndex);
+      print(finalIndex);
 
       chartData.insert(
           0,
           ChartItem(
-              convertDateToString(currentDate),
-              service.getAverage(Type.temperatureInside, tempArray),
-              service.getAverage(Type.temperatureOutside, tempArray),
-              service.getAverage(Type.humidityInside, tempArray),
-              service.getAverage(Type.humidityOutside, tempArray),
-              service.getAverage(Type.sound, tempArray)));
+              convertDateToString(dataStateTemp[initIndex].timestamp),
+              service.getAverage(Type.temperatureInside,
+                  dataStateTemp.sublist(initIndex, finalIndex)),
+              service.getAverage(Type.temperatureOutside,
+                  dataStateTemp.sublist(initIndex, finalIndex)),
+              service.getAverage(Type.humidityInside,
+                  dataStateTemp.sublist(initIndex, finalIndex)),
+              service.getAverage(Type.humidityOutside,
+                  dataStateTemp.sublist(initIndex, finalIndex)),
+              service.getAverage(
+                  Type.sound, dataStateTemp.sublist(initIndex, finalIndex))));
+
+      if (finalIndex + 1 < dataStateTemp.length) {
+        finalIndex += 1;
+        initIndex = finalIndex;
+        firstDate = dataStateTemp[initIndex].timestamp.day;
+      } else {
+        break;
+      }
+    }
+
+    while (chartData.length < 6) {
+      chartData.insert(0, ChartItem("", 0, 0, 0, 0, 0));
     }
     return chartData;
   }
